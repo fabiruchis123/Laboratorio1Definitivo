02e526f57e54dfff1fe6af6b0986e188a901406c
CHIKITIN19 <jimenezjeancarlos9@gmail.com>
2023-10-09 13:49:33 -0600

    Creacion de clase main
    Para crear cuentas dolares y colones

---
+++ src/Test/CrearCuentas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Test;
+
+import Account.Account;
+import Account.ColonAccount;
+import Account.DollarAccount;
+
+/**
+ *
+ * @author UTN
+ */
+public class CrearCuentas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Account[] cuentas = new Account[7];
+        DollarAccount[] cuentasDolares = new DollarAccount[7];
+        ColonAccount[] cuentasColones = new ColonAccount[7];
+        
+        
+        for (int i = 0; i < 7; i++) {
+//            cuentasDolares[i] = new DollarAccount();
+//            cuentasColones[i] = new ColonAccount();
+        }
+    }
+    
+}
